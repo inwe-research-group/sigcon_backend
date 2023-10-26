f9b100c9d26949409a9705ce70df2b950c23124d
inwe-research-group <zmamanir@unmsm.edu.pe>
2023-10-26 12:33:54 -0500

    add restcontroller anotacion demo

---
+++ src/test/java/uni/isw/sigcon/SigconApplicationTests.java
@@ -0,0 +1,13 @@
+package uni.isw.sigcon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigconApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
